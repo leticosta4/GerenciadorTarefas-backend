d011c619c54fc3647981a6e6f4ac7c978b64ce97
let <leticiacostaoa@gmail.com>
2024-07-07 22:18:34 -0300

    umas coisas ai

--- src/main/java/com/nossogrupo/GerenciadorTarefas/controller/TaskUserController.java
+++ src/main/java/com/nossogrupo/GerenciadorTarefas/controller/TaskUserController.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.nossogrupo.GerenciadorTarefas.model.TaskUser;
-import com.nossogrupo.GerenciadorTarefas.model.projection.UserProjection;
+//import com.nossogrupo.GerenciadorTarefas.model.projection.UserProjection;
 import com.nossogrupo.GerenciadorTarefas.repository.TaskUserRepository;
 import com.nossogrupo.GerenciadorTarefas.service.UserService;
 
